33eda5accf50af6d675dd6ef1f128596f9843c7e
VoChiDanh <vochidanh.hs.c@gmail.com>
2021-12-09 16:20:51 +0700

    Fix command

--- src/main/java/net/danh/diemsinhmenh/commands/commands.java
+++ src/main/java/net/danh/diemsinhmenh/commands/commands.java
@@ -27,7 +27,7 @@ public class commands implements CommandExecutor  {
                     sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Perm")));
                     sender.sendMessage("");
                     for (String helpplayer : main.getlang().getStringList("Help-player"))
-                    sender.sendMessage(main.convert(helpplayer));
+                        sender.sendMessage(main.convert(helpplayer));
                 }
                 if (sender.hasPermission("souls.admin")) {
                     for (String helpadmin : main.getlang().getStringList("Help-admin")) {
@@ -64,10 +64,15 @@ public class commands implements CommandExecutor  {
                     if (!sender.hasPermission("souls.admin")) {
                         sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Perm")));
                     }
-
                     if (sender.hasPermission("souls.admin")) {
-                        main.addLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
-                        sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Add-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        if (Bukkit.getPlayer(args[1]) == null) {
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Not-online")));
+                            return true;
+                        }
+                        if (Bukkit.getPlayer(args[1]) != null) {
+                            main.addLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Add-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        }
                     }
                 }
 
@@ -80,33 +85,47 @@ public class commands implements CommandExecutor  {
                             sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Not-online")));
                             return true;
                         }
-                        if (main.getLives(Bukkit.getPlayer(args[1])) < Integer.parseInt(args[2])) {
-                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Enough")));
-                            return true;
+                        if (Bukkit.getPlayer(args[1]) != null) {
+                            if (main.getLives(Bukkit.getPlayer(args[1])) < Integer.parseInt(args[2])) {
+                                sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Enough")));
+                                return true;
+                            }
+                            if (main.getLives(Bukkit.getPlayer(args[1])) >= Integer.parseInt(args[2])) {
+                                main.removeLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
+                                sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Take-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                            }
                         }
-
-                        main.removeLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
-                        sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Take-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
                     }
                 }
                 if (args[0].equalsIgnoreCase("set")) {
                     if (!sender.hasPermission("souls.admin")) {
                         sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Perm")));
                     }
-
                     if (sender.hasPermission("souls.admin")) {
-                        main.setLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
-                        sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Set-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        if (Bukkit.getPlayer(args[1]) == null) {
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Not-online")));
+                            return true;
+                        }
+                        if (Bukkit.getPlayer(args[1]) != null) {
+                            main.setLives(Bukkit.getPlayer(args[1]), Integer.parseInt(args[2]));
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Set-message")).replaceAll("%souls%", String.valueOf(Integer.parseInt(args[2]))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        }
                     }
                 }
                 if (args[0].equalsIgnoreCase("reset")) {
                     if (!sender.hasPermission("souls.admin")) {
                         sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Perm")));
                     }
-
                     if (sender.hasPermission("souls.admin")) {
-                        main.resetLives(Bukkit.getPlayer(args[1]));
-                        sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Reset-message")).replaceAll("%souls%", String.valueOf(main.getConfig().getInt("General.Normal"))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        if (Bukkit.getPlayer(args[1]) == null) {
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Not-online")));
+                            return true;
+                        }
+
+                        if (Bukkit.getPlayer(args[1]) != null) {
+                            main.resetLives(Bukkit.getPlayer(args[1]));
+                            sender.sendMessage(main.convert(main.getConfig().getString("prefix") + main.getlang().getString("lang." + main.getConfig().getString("language") + "." + "Reset-message")).replaceAll("%souls%", String.valueOf(main.getConfig().getInt("General.Normal"))).replaceAll("%player%", Bukkit.getPlayer(args[1]).getName()));
+                        }
                     }
                 }
             }
